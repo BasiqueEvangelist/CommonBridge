a69a048a0c1ac7961ad87238a680ea55a7d33154
Basique <basiqueevangelist@yandex.ru>
2023-12-07 00:08:46 +0300

    initial 1.20.2 port
    minecraft segfaulted when I tried to test this lmao

--- src/main/java/me/basiqueevangelist/commonbridge/CommonBridge.java
+++ src/main/java/me/basiqueevangelist/commonbridge/CommonBridge.java
@@ -1,17 +1,10 @@
 package me.basiqueevangelist.commonbridge;
 
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
-import com.mojang.authlib.GameProfile;
-import me.basiqueevangelist.commonbridge.ftbchunks.FtbChunksProtectionProvider;
 import me.basiqueevangelist.commonbridge.numismatic.NumismaticEconomyProvider;
 import me.basiqueevangelist.commonbridge.opac.OpacProtectionProvider;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
-
-import java.util.concurrent.TimeUnit;
 
 public class CommonBridge implements ModInitializer {
     @Override
@@ -19,13 +12,8 @@ public class CommonBridge implements ModInitializer {
         if (FabricLoader.getInstance().isModLoaded("numismatic-overhaul"))
             NumismaticEconomyProvider.init();
 
-        if (FabricLoader.getInstance().isModLoaded("ftbchunks"))
-            FtbChunksProtectionProvider.init();
-
         if (FabricLoader.getInstance().isModLoaded("openpartiesandclaims"))
             OpacProtectionProvider.init();
-
-        FakePlayerProvider.init();
     }
 
     public static Identifier id(String path) {
--- src/main/java/me/basiqueevangelist/commonbridge/FakePlayerProvider.java
+++ src/main/java/me/basiqueevangelist/commonbridge/FakePlayerProvider.java
@@ -1,48 +1,14 @@
 package me.basiqueevangelist.commonbridge;
 
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
 import com.mojang.authlib.GameProfile;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.fabricmc.fabric.api.entity.FakePlayer;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.concurrent.TimeUnit;
-
 public class FakePlayerProvider {
-    private static LoadingCache<GameProfile, ServerPlayerEntity> FAKE_PLAYERS = null;
-    private static int tickCount = 0;
-
-    public static void init() {
-        ServerLifecycleEvents.SERVER_STARTING.register(server -> {
-            FAKE_PLAYERS = CacheBuilder.newBuilder()
-                .initialCapacity(64)
-                .expireAfterWrite(
-                    30, TimeUnit.SECONDS)
-                .softValues()
-                .build(CacheLoader.from((profile) -> new ServerPlayerEntity(server, server.getOverworld(), profile)));
-        });
-
-        ServerTickEvents.END_SERVER_TICK.register(server -> {
-            tickCount++;
-
-            if (tickCount % 16 == 0)
-                FAKE_PLAYERS.cleanUp();
-        });
-
-        ServerLifecycleEvents.SERVER_STOPPED.register(server -> {
-            FAKE_PLAYERS.invalidateAll();
-            FAKE_PLAYERS = null;
-        });
-    }
-
     public static @Nullable ServerPlayerEntity tryResolveProtectionPlayer(World w, GameProfile profile, PlayerEntity old) {
         if (old instanceof ServerPlayerEntity spe)
             return spe;
@@ -54,6 +20,6 @@ public class FakePlayerProvider {
 
         if (online != null) return online;
 
-        return FAKE_PLAYERS.getUnchecked(profile);
+        return FakePlayer.get(sw, profile);
     }
 }
