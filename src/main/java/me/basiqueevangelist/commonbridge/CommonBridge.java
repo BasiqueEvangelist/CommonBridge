a5c12236556edebfc6b40abcb7dd2b99e52714e9
Basique <basiqueevangelist@yandex.ru>
2023-12-09 20:28:49 +0300

    add opt outs and log messages

--- src/main/java/me/basiqueevangelist/commonbridge/CommonBridge.java
+++ src/main/java/me/basiqueevangelist/commonbridge/CommonBridge.java
@@ -7,24 +7,53 @@ import me.basiqueevangelist.commonbridge.opac.OpacProtectionProvider;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CommonBridge implements ModInitializer {
+    private static final Logger LOGGER = LoggerFactory.getLogger("CommonBridge");
+
     @Override
     public void onInitialize() {
-        if (FabricLoader.getInstance().isModLoaded("numismatic-overhaul"))
+        if (shouldEnable("numismatic-overhaul"))
             NumismaticEconomyProvider.init();
 
-        if (FabricLoader.getInstance().isModLoaded("openpartiesandclaims"))
+        if (shouldEnable("openpartiesandclaims"))
             OpacProtectionProvider.init();
 
-        if (FabricLoader.getInstance().isModLoaded("diamondeconomy"))
+        if (shouldEnable("diamondeconomy"))
             DiamondEconomyProvider.init();
 
-        if (FabricLoader.getInstance().isModLoaded("lightmanscurrency"))
+        if (shouldEnable("lightmanscurrency"))
             LightmansEconomyProvider.init();
+
+        LOGGER.info("Common Bridge initialized.");
     }
 
     public static Identifier id(String path) {
         return new Identifier("common-bridge", path);
+    }
+
+    public static boolean shouldEnable(String modId) {
+        var mod = FabricLoader.getInstance().getModContainer(modId).orElse(null);
+
+        if (mod == null) {
+            LOGGER.info("Skipping provider for {}, as it is not installed", modId);
+            return false;
+        }
+
+        if (mod.getMetadata().containsCustomValue("common-bridge:opt_out")) {
+            LOGGER.info("Skipping provider for {}, as it has manually opted out", modId);
+            return false;
+        }
+
+        String propKey = "commonBridge." + modId;
+        if (System.getProperty(propKey) != null && !Boolean.getBoolean(propKey)) {
+            LOGGER.info("Skipping provider for {}, as the user has manually opted out", modId);
+            return false;
+        }
+
+        LOGGER.info("Enabling provider for {}", modId);
+        return true;
     }
 }
